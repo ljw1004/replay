6b0e08a71192357c25b1b1828ccdc6c4b6508b1e
ljw1004 <lu@wischik.com>
2016-07-12 07:18:38 -0700

    In process of redoing the editor <-> host <-> client protocol

--- ReplayClient.cs
+++ ReplayClient.cs
@@ -10,31 +10,57 @@ namespace System.Runtime.CompilerServices
 {
     public class Replay
     {
-        // < STAMP stamp
-        // > WATCH file line count stamp
-        // < REPLAY add id line txt
-        // < REPLAY remove id
-        // > DUMP stamp
+        // Client <-> Host <-> Editor
+        // The host is always on the same machine as the replayer. They communicate by serialization.
+        // The host and editor are in the same process (VSIX) or on remote machines (online).
+        // The host can trigger the client to shut down and a new client to launch.
+        
+        // For now:
+        // CLIENT: Each successive instance of the client builds up a database of replays that it's executed
+        // so far. It knows which range the host is currently watching, and if it further executes anything within that
+        // range, it notifies the host.
+        // HOST: The host builds up a database of replays, a database that persists across multiple client instances.
+        // For each entry in that database it assigns a tracking ID. It ensures the editor has the same database,
+        // by notifying the editor of each removed/added entry and associated ID. It knows which range the editor
+        // is currently watching. If the range changes, by assumption the editor already knows the host's database.
+        // The host also tells the client about the new range
+        // 
+        // Host builds up a database of ranges it knows, and hashes+text of the replays within that range,
+        // and a synthesized ID for each replay.
+        // Client 
+
+        // Host keeps a list of ranges it knows, and hashes+texts of the replays within that range.
+        // Assumption is that the client knows everything that the host does.
+        // When editor requests a range, host delivers what it can and requests the rest from the client
+        // When host requests a range, it sends all the lines within that range that it knows;
+        // client will reply back by removing and adding as necessary.
+        // When client updates asynchronously, it will only send back if within the watched range.
+        
+
+        // > WATCH file line count nhashes line1 hash1 ... lineN hashN
+        // < REPLAY add line hash txt
+        // < REPLAY remove line hash
+        // > DUMP
         // < DUMP file line txt
 
         static TextWriter SystemOut = Console.Out;
         static TextReader SystemIn = Console.In;
         static HookOut MyOut = new HookOut();
         static HookIn MyIn = new HookIn();
-        static Dictionary<string, Dictionary<int, LinkedList<LineItem>>> Database = new Dictionary<string, Dictionary<int, LinkedList<LineItem>>>();
+        static Dictionary<string, Dictionary<int, LineItem>> Database = new Dictionary<string, Dictionary<int, LineItem>>();
         static BufferBlock<LineItem> Queue = new BufferBlock<LineItem>();
-        static HashSet<LineItem> CurrentLines = new HashSet<LineItem>();
-        static long Stamp = 0;
-        static long Id = 0;
 
         private struct LineItem
         {
-            public long Id;
-            public string File;
-            public int Line;
-            public long Stamp;
-            public string Text;
-            public LineItem WithId(long id) => new LineItem { Id = id, File = File, Line = Line, Stamp = Stamp, Text = Text };
+            public readonly string File;
+            public readonly int Line;
+            public readonly string Text;
+            public readonly int TextHash;
+            
+            public LineItem(string file, int line, string text)
+            {
+                File = file; Line = line; Text = text; TextHash = text?.GetHashCode() ?? 0;
+            }
         }
 
         static Replay()
@@ -58,30 +84,11 @@ namespace System.Runtime.CompilerServices
         {
         }
 
-        private static LineItem? GetLineItem(LinkedList<LineItem> lineItems, long stamp)
-        {
-            if (lineItems == null || lineItems.Count == 0) return null;
-            if (stamp == 0) return lineItems.Last.Value;
-            LinkedListNode<LineItem> node = null;
-            for (node = lineItems.Last; node != null; node = node.Previous)
-            {
-                if (node.Value.Stamp <= stamp) break;
-            }
-            return node?.Value;
-        }
-
-        class LineItemComparer : IEqualityComparer<LineItem>
-        {
-            public bool Equals(LineItem x, LineItem y) => x.File == y.File && x.Line == y.Line && x.Stamp == y.Stamp;
-            public int GetHashCode(LineItem x) => unchecked (((17 * 23 + x.File.GetHashCode()) * 23 + x.Line) * 23 + x.Stamp.GetHashCode());
-        }
-        static LineItemComparer Comparer = new LineItemComparer();
-
         public static void RunConversation()
         {
             var queueTask = Queue.ReceiveAsync();
             var stdinTask = Task.Run(SystemIn.ReadLineAsync);
-            string watchFile = null; int watchLine = -1, watchCount = -1; long watchStamp = -1;
+            string watchFile = null; int watchLine = -1, watchCount = -1;
             SystemOut.WriteLine("OK");
             while (true)
             {
@@ -101,58 +108,60 @@ namespace System.Runtime.CompilerServices
 
                     else if (cmds[0] == "DUMP")
                     {
-                        long stamp = 0;
-                        if ((cmds.Count != 1 && cmds.Count != 2)
-                            || (cmds.Count == 2 && !long.TryParse(cmds[1], out stamp)))
+                        if (cmds.Count != 1)
                         {
-                            SystemOut.WriteLine($"ERROR\tExpected 'DUMP [stamp]', got '{line}'"); continue;
+                            SystemOut.WriteLine($"ERROR\tExpected 'DUMP', got '{line}'"); continue;
                         }
                         foreach (var kv1 in Database)
                         {
                             foreach (var kv2 in kv1.Value)
                             {
-                                var lineItem = GetLineItem(kv2.Value, stamp);
-                                if (lineItem.HasValue) SystemOut.WriteLine($"DUMP\t{lineItem.Value.File}\t{lineItem.Value.Line}\t{lineItem.Value.Stamp}\t{lineItem.Value.Text}");
+                                var lineItem = kv2.Value;
+                                SystemOut.WriteLine($"DUMP\t{lineItem.File}\t{lineItem.Line}\t{lineItem.Text}");
                             }
                         }
                     }
 
                     else if (cmds[0] == "WATCH")
                     {
-                        string file; int iline, icount; long stamp = 0;
-                        if ((cmds.Count != 4 && cmds.Count != 5)
+                        string file; int iline, icount, nhashes, ihash;
+                        if (cmds.Count < 5
                             || (file = cmds[1]) == null
                             || !int.TryParse(cmds[2], out iline) || !int.TryParse(cmds[3], out icount)
-                            || (cmds.Count == 5 && (!long.TryParse(cmds[4], out stamp) || stamp >= Stamp)))
+                            || !int.TryParse(cmds[4], out nhashes)
+                            || cmds.Count != nhashes*2 + 5)
                         {
-                            SystemOut.WriteLine($"ERROR\tExpected 'WATCH file line count [stamp]', got '{line}'"); continue;
+                            SystemOut.WriteLine($"ERROR\tExpected 'WATCH file line count nhashes ...', got '{line}'"); continue;
                         }
-                        watchFile = file; watchLine = iline; watchCount = icount; watchStamp = stamp;
+                        var hostAlreadyHas = new Dictionary<int, int>();
+                        for (int i=0; i<nhashes; i++)
+                        {
+                            if (!int.TryParse(cmds[5+i], out iline) || !int.TryParse(cmds[6+i], out ihash)) { SystemOut.WriteLine($"ERROR\twrong hash #{i} in '{line}'"); continue; }
+                            hostAlreadyHas[iline] = ihash;
+                        }
+                        watchFile = file; watchLine = iline; watchCount = icount;
                         //
-                        var newLines = new HashSet<LineItem>();
+                        var hostWillHave = new Dictionary<int,LineItem>();
                         if (Database.ContainsKey(watchFile))
                         {
                             var dbfile = Database[watchFile];
                             foreach (var kv in dbfile)
                             {
                                 if (kv.Key < watchLine || kv.Key >= watchLine + watchCount) continue;
-                                var lineItem = GetLineItem(kv.Value, stamp);
-                                if (lineItem.HasValue) newLines.Add(lineItem.Value.WithId(Id++));
+                                hostWillHave[kv.Key] = kv.Value;
                             }
                         }
                         //
-                        var toRemove = CurrentLines.Except(newLines, Comparer);
-                        var toAdd = newLines.Except(CurrentLines, Comparer);
-                        foreach (var li in toRemove)
+                        var toRemove = hostAlreadyHas;
+                        var toAdd = new List<LineItem>();
+                        foreach (var kv in hostWillHave)
                         {
-                            SystemOut.WriteLine($"REPLAY\tremove\t{li.Id}");
+                            int hash; bool wasLineInHost = toRemove.TryGetValue(kv.Key, out hash);
+                            if (wasLineInHost && hash == kv.Value.TextHash) { toRemove.Remove(kv.Key); continue; } // no need for update
+                            toAdd.Add(kv.Value);
                         }
-                        foreach (var li in toAdd)
-                        {
-                            SystemOut.WriteLine($"REPLAY\tadd\t{li.Id}\t{li.Line}\t{li.Text}");
-                        }
-                        CurrentLines = newLines;
-
+                        foreach (var kv in toRemove) SystemOut.WriteLine($"REPLAY\tremove\t{kv.Key}\t{kv.Value}");
+                        foreach (var li in toAdd) SystemOut.WriteLine($"REPLAY\tadd\t{li.Line}\t{li.TextHash}\t{li.Text}");
                     }
 
                     else
@@ -165,18 +174,15 @@ namespace System.Runtime.CompilerServices
 
                 if (queueTask.IsCompleted)
                 {
-                    // TODO: should ID be added to the lineitem here rather than in Log<>?
                     var li = queueTask.Result;
                     queueTask = Queue.ReceiveAsync();
-                    if (!Database.ContainsKey(li.File)) Database[li.File] = new Dictionary<int, LinkedList<LineItem>>();
+                    if (!Database.ContainsKey(li.File)) Database[li.File] = new Dictionary<int, LineItem>();
                     var dbfile = Database[li.File];
-                    if (!dbfile.ContainsKey(li.Line)) dbfile[li.Line] = new LinkedList<LineItem>();
-                    var lineItems = dbfile[li.Line];
-                    lineItems.AddLast(li);
+                    dbfile[li.Line] = li;
                     //
-                    if (watchStamp == 0 && watchFile == li.File && watchLine <= li.Line && watchLine + watchCount > li.Line)
+                    if (watchFile == li.File && watchLine <= li.Line && watchLine + watchCount > li.Line)
                     {
-                        SystemOut.WriteLine($"REPLAY\tadd\t{li.Id}\t{li.Line}\t{li.Text}");
+                        SystemOut.WriteLine($"REPLAY\tadd\t{li.Line}\t{li.TextHash}\t{li.Text}");
                     }
                 }
             }
@@ -188,16 +194,14 @@ namespace System.Runtime.CompilerServices
             // Empty arguments is used purely to ensure the static constructor of Replay has been run
             if (data == null && id == null && file == null) return default(T);
 
-            long stamp = Interlocked.Increment(ref Stamp);
-
             // Gather all the pending Console.Writes from the app, via our hooked side-effect Console.Out monoid
             var s = MyOut.Log()?.Replace("\\", "\\\\").Replace("\r", "\\r").Replace("\n", "\\n");
-            if (s != null) Queue.Post(new LineItem { File = file, Line = line, Stamp = stamp, Text = $"\"{s}\"" });
+            if (s != null) Queue.Post(new LineItem(file, line, $"\"{s}\""));
 
             // Log this current event (for declarations and expression statements)
             s = (id == null) ? "" : id + "=";
             s += (data == null) ? "null" : data.ToString();
-            if (reason == 1 || reason == 2) Queue.Post(new LineItem { File = file, Line = line, Stamp = stamp, Text = s });
+            if (reason == 1 || reason == 2) Queue.Post(new LineItem(file, line, s));
 
             return data;
         }
